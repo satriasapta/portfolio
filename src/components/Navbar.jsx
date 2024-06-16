60559f631fdd796b7608ebac9def1d20e1b6e770
Satria Sapta <satriasapta48@gmail.com>
2024-06-17 01:04:56 +0700

    change logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     return (
         <nav className=" mb-20 flex items-center justify-between py-6">
             <div className="flex flex-shrink-0 items-center">
-                <img className="mx-2 w-10" src={logo} alt="logo" />
+                <h1 className="text-3xl font-bold">S S</h1>
             </div>
             <div className="m-8 flex items-center justify-center gap-4 text-2xl">
                 <FaLinkedin />
